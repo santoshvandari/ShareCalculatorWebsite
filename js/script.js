4924058dd1b80fdfe9fc09e4c9fe36a4bdc30c38
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-11 10:52:50 +0545

    Initial

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 'use strict';
 (()=>{
+    // selecting all the Element 
     let preloaderEl= document.querySelector('div.preloader');
     let selectoptEl= document.getElementById('selectopt');
     let buyFormDiv=document.querySelector("div.buy-section");
     let sellFormDiv=document.querySelector("div.sell-section");
     let resultDiv=document.querySelector("div.calculated-result");
+    let errorDiv= document.querySelector("div.error");
+    let sellForm = document.querySelector('div.sell-form form');
+    let buyForm = document.querySelector('div.buy-form form');
+    errorDiv.textContent='* are Mandatory field.';
+    // Handeling PReloader 
     window.addEventListener('load',()=>{
         preloaderEl.style.display='none';
     });
-    console.log(selectoptEl)
+    // Handeling Option Selection 
     selectoptEl.addEventListener('change',()=>{
         let selectionElValue=selectoptEl.value;
         if(selectionElValue==='buy'){
@@ -19,4 +25,14 @@
             sellFormDiv.style.display='block';
         }
     });
+    // Handeling the Sell Calculation 
+    console.log(sellForm);
+    sellForm.addEventListener('submit',(e)=>{
+        e.preventDefault();
+        console.log("hello")
+    });
+
+
+
+    // Handeling the Sell Calculation 
 })();
