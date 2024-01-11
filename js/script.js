8ad8925ce712732eab336f22ac3748ab42f362db
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2024-01-11 10:45:14 +0545

    Initial

--- js/script.js
+++ js/script.js
@@ -2,8 +2,21 @@
 (()=>{
     let preloaderEl= document.querySelector('div.preloader');
     let selectoptEl= document.getElementById('selectopt');
+    let buyFormDiv=document.querySelector("div.buy-section");
+    let sellFormDiv=document.querySelector("div.sell-section");
+    let resultDiv=document.querySelector("div.calculated-result");
     window.addEventListener('load',()=>{
         preloaderEl.style.display='none';
     });
-    selectoptEl.addEventListener
+    console.log(selectoptEl)
+    selectoptEl.addEventListener('change',()=>{
+        let selectionElValue=selectoptEl.value;
+        if(selectionElValue==='buy'){
+            buyFormDiv.style.display='block';
+            sellFormDiv.style.display='none';
+        }else if(selectionElValue==='sell'){
+            buyFormDiv.style.display='none';
+            sellFormDiv.style.display='block';
+        }
+    });
 })();
